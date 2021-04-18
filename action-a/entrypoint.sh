21dd51fd0241c59aeab42cd24ce64d93b1617de3
Martin Liu <liuzh66@gmail.com>
2021-04-18 13:14:16 +0800

    Update entrypoint.sh
    fix snyctax error

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
-sh -c "echo \nThis is a line from a shell script execurted from a Docker image. 
+sh -c "echo \nThis is a line from a shell script execurted from a Docker image. "
